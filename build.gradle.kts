064b1b4acfa35947a1dd57ac9c4a847ea780c67e
Julian Sobott <julian.sobott@gmail.com>
2020-08-03 22:03:46 +0200

    Transfer from old repository

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+    `java-gradle-plugin`
+    `maven-publish`
+    id("com.gradle.plugin-publish") version "0.12.0"
+    kotlin("plugin.serialization") version "1.3.70"
+    id("net.anshulverma.gradle.fileupload") version "1.0.0"
+}
+
+version = "0.1"
+
+dependencies {
+    implementation(kotlin("stdlib", org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
+    implementation(group = "io.github.rybalkinsd", name = "kohttp", version = "0.12.0")
+    implementation("net.anshulverma.gradle:gradle-fileupload-plugin:1.0.4")
+}
+
+gradlePlugin {
+    plugins {
+        create("studyToolPlugin") {
+            id = "com.github.juliansobott.studytoolplugin"
+            displayName = "Study Tool Plugin"
+            description = "Develop and publish your own plugin for the 'StudyTool'."
+            implementationClass = "com.github.juliansobott.studytoolplugin.StudyToolPlugin"
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.juliansobott.studytoolplugin"
+            artifactId = "StudyToolPlugin"
+            version = "0.1"
+
+            from(components["kotlin"])
+        }
+    }
+}
+
+
+
+pluginBundle {
+    website = "https://github.com/JulianSobott/TimeManager"
+    vcsUrl = "https://github.com/JulianSobott/TimeManager"
+    tags = listOf("studymanager", "plugin", "java")
+}
+
---
+++ src/main/kotlin/com/github/juliansobott/studytoolplugin/StudyToolPlugin.kt
@@ -0,0 +1,204 @@
+package com.github.juliansobott.studytoolplugin
+
+import groovy.lang.Closure
+import io.github.rybalkinsd.kohttp.dsl.upload
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import net.anshulverma.gradle.fileupload.FileUploadExtension
+import org.codehaus.groovy.ast.tools.GeneralUtils.params
+import org.gradle.api.DefaultTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.api.tasks.bundling.Zip
+import java.io.File
+import java.io.FileNotFoundException
+
+/**
+ * Tasks:
+ * - publish plugin
+ * - Check plugin
+ * - test plugin installed
+ * - test plugin not installed
+ */
+
+const val GROUP = "study tool"
+
+const val TASK_PUBLISH = "publishPlugin"
+const val TASK_BUILD_PUBLISH = "buildPublish"
+const val TASK_CHECK = "checkPlugin"
+const val TASK_TEST = "testPlugin"
+const val TASK_INIT = "initProject"
+
+const val META_FILE = "plugin_load.json"
+
+class StudyToolPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        // Plugin dependencies
+        project.plugins.apply("java")
+        project.plugins.apply("net.anshulverma.gradle.fileupload")
+
+        // Extension
+        val ext = project.extensions.create<StudyToolExtension>("StudyTool", StudyToolExtension::class.java)
+
+        // Config
+        val extJar = project.tasks.getByName("jar")
+
+
+        // Register tasks
+        project.tasks.register<BuildPublishTask>(TASK_BUILD_PUBLISH, BuildPublishTask::class.java)
+        project.tasks.register<PublishTask>(TASK_PUBLISH, PublishTask::class.java)
+        project.tasks.register<CheckTask>(TASK_CHECK, CheckTask::class.java)
+        project.tasks.register<TestPluginTask>(TASK_TEST, TestPluginTask::class.java)
+        project.tasks.register<InitProjectFilesTask>(TASK_INIT, InitProjectFilesTask::class.java)
+
+        // Edit tasks
+        project.tasks.getByPath("jar").doFirst {
+            if (it is Jar) {
+                it.archiveFileName.set("${ext.id}.jar")
+                // create meta file
+                val json = Json(JsonConfiguration.Stable)
+                val data = json.stringify(LoadDataExtension.serializer(), ext.loadDataSettings)
+                println(data)
+                val path = "build/resources/$META_FILE"
+                File(path).writeText(data)
+                // add meta file
+                it.from(path)
+            } else {
+                // Empty: Project is wrong configured
+            }
+        }
+
+        // Task dependencies
+        project.tasks.getByPath(TASK_BUILD_PUBLISH).dependsOn("jar", TASK_CHECK)
+        project.tasks.getByPath(TASK_PUBLISH).dependsOn(TASK_BUILD_PUBLISH)
+        project.tasks.getByPath(TASK_PUBLISH).finalizedBy(project.tasks.getByPath("fileupload"))
+    }
+}
+
+open class CheckTask : DefaultTask() {
+
+    init {
+        group = GROUP
+    }
+
+    @TaskAction
+    fun checkPlugin() {
+        println("Hello: Checking!")
+        // Get Meta file
+        // Check existence of fxml paths
+        // Check existence of info files
+    }
+}
+
+open class TestPluginTask : DefaultTask() {
+
+    init {
+        group = GROUP
+    }
+
+    @TaskAction
+    fun testPlugin() {
+        println("Hello: testing!")
+    }
+}
+
+open class PublishTask : DefaultTask() {
+
+    init {
+        group = GROUP
+    }
+
+    @TaskAction
+    fun publishStudyPlugin() {
+        println("Hello: Publishing!")
+        val task = project.tasks.getByPath(TASK_BUILD_PUBLISH)
+        val ext = project.extensions.getByType(StudyToolExtension::class.java)
+
+        if (task is BuildPublishTask) {
+            val filePath = File("${task.destinationDirectory.get()}", task.archiveFileName.get())
+            val fileUploadExt = project.extensions.getByType(FileUploadExtension::class.java)
+            fileUploadExt.url = "http://127.0.0.1:8080/plugins/upload"
+            fileUploadExt.file = filePath.path
+            fileUploadExt.params["name"] = ext.id
+        }
+    }
+}
+
+
+open class BuildPublishTask : Zip() {
+
+    init {
+        group = GROUP
+
+        archiveBaseName.set("publish")
+        destinationDirectory.set(File("build/"))
+
+        // info files TODO: get extensions value
+        val ext = project.extensions.getByType(StudyToolExtension::class.java)
+        val infoPath = project.projectDir.path.plus("\\${ext.infoPath}")
+        val infoFiles = File(infoPath)
+        if (!infoFiles.exists()) {
+            throw FileNotFoundException("Info folder not found: $infoPath")
+        }
+        into("info") { inner ->
+            inner.from(infoFiles)
+        }
+
+        // jar file
+        from(File("build/libs"))
+    }
+}
+
+open class InitProjectFilesTask : DefaultTask() {
+
+    init {
+        group = GROUP
+    }
+
+    @TaskAction
+    fun initFiles() {
+        val mapping = mapOf(
+                "MainController.java" to "src/main/java/MainController.java",
+                "SettingsController.java" to "src/main/java/SettingsController.java",
+                "main.fxml" to "src/main/resources/main.fxml",
+                "settings.fxml" to "src/main/resources/settings.fxml",
+                "README.md" to "README.md",
+                "tab_info.yaml" to "info/tab_info.yaml",
+                "gitignore.txt" to ".gitignore" // .txt because otherwise can't find resource
+        )
+        for (e in mapping.entries) {
+            val sourceStream = javaClass.getResourceAsStream("/templates/${e.key}")
+            val dest = project.file(e.value)
+            if (!dest.exists()) {
+                dest.parentFile.mkdirs()
+                dest.writeBytes(sourceStream.readAllBytes())
+                println("Successfully created: ${e.value}")
+            } else {
+                println("Already exists: ${e.value}")
+            }
+        }
+    }
+}
+
+
+open class StudyToolExtension {
+    var infoPath: String = "info"
+    var id: String = "TestID"
+    var loadDataSettings: LoadDataExtension = LoadDataExtension()
+
+    fun loadData(closure: Closure<Any>) {
+        loadDataSettings = LoadDataExtension()
+        closure.delegate = loadDataSettings
+        closure.call()
+    }
+}
+
+@Serializable
+open class LoadDataExtension {
+    var windowFxml: String = "main.fxml"
+    var settingsFxml: String = "settings.fxml"
+    var icon: String = "icon.png"
+}
