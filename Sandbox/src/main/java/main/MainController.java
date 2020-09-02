439dee11de83ed148585a79c43773faa7ee76dc5
Julian Sobott <julian.sobott@gmail.com>
2020-09-02 14:19:40 +0200

    Add dummy Button for fun

--- Sandbox/src/main/java/main/MainController.java
+++ Sandbox/src/main/java/main/MainController.java
@@ -1,6 +1,8 @@
 package main;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,8 +10,15 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
+    @FXML
+    Button btnDummy;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Hello world");
+        btnDummy.setOnMouseClicked(e -> {
+            System.out.println("Clicked a button");
+            btnDummy.setText("You clicked me");
+        });
     }
 }
